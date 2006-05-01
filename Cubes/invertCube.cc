cfefdb294535451d2535b0c4ceff5c1860a88402
whi550 <whi550@450e7745-72da-457f-8a74-b07dc0cda934>
2006-05-01 00:01:18 +0000

    Added line to reInvertCube that inverts the recon array as well.
    git-svn-id: http://svn.atnf.csiro.au/duchamp/trunk@61 450e7745-72da-457f-8a74-b07dc0cda934

--- Cubes/invertCube.cc
+++ Cubes/invertCube.cc
@@ -19,8 +19,12 @@ void Cube::reInvert()
    *   Any objects will have the flux of each pixel inverted as well.
    *   This is used when searching for negative features.
    */
-  for(int i=0; i<this->numPixels; i++)
-    if(!this->isBlank(i)) this->array[i] *= -1.;
+  for(int i=0; i<this->numPixels; i++){
+    if(!this->isBlank(i)){
+      this->array[i] *= -1.;
+      if(this->reconExists) this->recon[i] *= -1.;
+    }
+  }
 
   for(int i=0; i<this->objectList.size(); i++){
     for(int pix=0; pix<this->objectList[i].getSize(); pix++){
