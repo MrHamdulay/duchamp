8cd4aec2e19d5507c0aa5d1d39a1e89b5de52aab
whi550 <whi550@450e7745-72da-457f-8a74-b07dc0cda934>
2006-05-09 04:05:45 +0000

    Updated Utils/cpgcumul.c to better deal with the pgplot box/environment, and to properly access the sort() routine.
    git-svn-id: http://svn.atnf.csiro.au/duchamp/trunk@68 450e7745-72da-457f-8a74-b07dc0cda934

--- Utils/cpgcumul.c
+++ Utils/cpgcumul.c
@@ -8,28 +8,22 @@ void cpgcumul(int npts, float *data, float datamin, float datamax, int pgflag)
 {
   int i;
   float *sorted;
-  float x1,x2,y1,y2;
   float MINCOUNT=0.;
 
   sorted = (float *)calloc(npts,sizeof(float));
   for(i=0;i<npts;i++) sorted[i] = data[i];
-  _sort(sorted,0,npts-1);
+  _sort(sorted,0,npts);
 
   cpgbbuf();
 
-  // BOUNDARIES OF PLOT
-  x1 = datamin;
-  x2 = datamax;
-  y1 = 0.;
-  y2 = 1.001;
-
   // DEFINE ENVIRONMENT IF NECESSARY
-  if(pgflag%2 == 0) cpgenv(x1,x2,y1,y2,0,0);
+  if(pgflag == 0) cpgenv(datamin,datamax,MINCOUNT,1.0001,0,0);
+  if(pgflag == 2) cpgswin(datamin,datamax,MINCOUNT,1.);
 
   // DRAW LINE
   cpgmove(datamin,MINCOUNT);
   for(i=0;i<npts;i++) cpgdraw(sorted[i],(float)(i+1)/(float)(npts));
-  //  cpgdraw(datamax,1.);
+  cpgdraw(datamax,1.);
 
   cpgebuf();
   
