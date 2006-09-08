c1d68f4cc31a9223c9ab3ea7a197c4ae7740bcf0
whi550 <whi550@450e7745-72da-457f-8a74-b07dc0cda934>
2006-09-08 00:22:01 +0000

    Further updates to the verification files.
    git-svn-id: http://svn.atnf.csiro.au/duchamp/trunk@152 450e7745-72da-457f-8a74-b07dc0cda934

--- VerifyDuchamp.sh
+++ VerifyDuchamp.sh
@@ -19,7 +19,7 @@ echo "Done. Comparison to standard result:"
 echo "Differences with Results:"
 diff -I"Results of the Duchamp source finder:" verification/results1.txt verification/stdResults1.txt
 echo "Differences with Log:"
-diff -I"New run of the Duchamp sourcefinder" verification/log1.ps verification/stdLog1.ps
+diff -I"New run of the Duchamp sourcefinder" verification/log1.txt verification/stdLog1.txt
 
 echo 
 echo "Running the second Duchamp test:"
@@ -30,7 +30,7 @@ echo "Done. Comparison to standard result:"
 echo "Differences with Results:"
 diff -I"Results of the Duchamp source finder:" verification/results2.txt verification/stdResults2.txt
 echo "Differences with Log:"
-diff -I"CreationDate" verification/log2.ps verification/stdLog2.ps
+diff -I"New run of the Duchamp sourcefinder" verification/log2.txt verification/stdLog2.txt
 
 echo 
 echo "Running the third Duchamp test:"
@@ -43,6 +43,6 @@ echo "Done. Comparison to standard result:"
 echo "Differences with Results:"
 diff -I"Results of the Duchamp source finder:" verification/results3.txt verification/stdResults3.txt
 echo "Differences with Log:"
-diff -I"CreationDate" verification/log3.ps verification/stdLog3.ps
+diff -I"New run of the Duchamp sourcefinder" verification/log3.txt verification/stdLog3.txt
 
 echo "No differences? Then everything worked as expected\!"
