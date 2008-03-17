119d927d390744fbd1b8ba934d8f0474e282ab97
MatthewWhiting <MatthewWhiting@450e7745-72da-457f-8a74-b07dc0cda934>
2008-03-17 23:12:50 +0000

    Updated verification results to account for new beam sizes etc.
    git-svn-id: http://svn.atnf.csiro.au/duchamp/trunk@429 450e7745-72da-457f-8a74-b07dc0cda934

--- src/Detection/detection.cc
+++ src/Detection/detection.cc
@@ -413,9 +413,7 @@ namespace duchamp
 
     if(head.isWCS()){
       // correct for the beam size if the flux units string ends in "/beam"
-      if(head.needBeamSize())
-// 	this->intFlux  *= double(this->getSpatialSize())/head.getBeamSize();
-	this->intFlux  /= head.getBeamSize();
+      if(head.needBeamSize()) this->intFlux  /= head.getBeamSize();
     }
 
   }
@@ -503,9 +501,7 @@ namespace duchamp
 
     if(head.isWCS()){
       // correct for the beam size if the flux units string ends in "/beam"
-      if(head.needBeamSize())
-// 	this->intFlux  *= double(this->getSpatialSize())/head.getBeamSize();
-	this->intFlux  /= head.getBeamSize();
+      if(head.needBeamSize()) this->intFlux  /= head.getBeamSize();
     }
 
   }
