9ee5ce76f90e11c749757bdad1b624680c3d1253
MatthewWhiting <MatthewWhiting@450e7745-72da-457f-8a74-b07dc0cda934>
2012-10-08 03:04:52 +0000

    Adding the licensing text to files that didn't have it.
    git-svn-id: http://svn.atnf.csiro.au/duchamp/trunk@1069 450e7745-72da-457f-8a74-b07dc0cda934

--- src/Outputs/CatalogueWriter.cc
+++ src/Outputs/CatalogueWriter.cc
@@ -1,4 +1,39 @@
+// -----------------------------------------------------------------------
+// CatalogueWriter.cc: Implementation of the base class for writing
+//                     results to output files
+// -----------------------------------------------------------------------
+// Copyright (C) 2006, Matthew Whiting, ATNF
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2 of the License, or (at your
+// option) any later version.
+//
+// Duchamp is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Duchamp; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA
+//
+// Correspondence concerning Duchamp may be directed to:
+//    Internet email: Matthew.Whiting [at] atnf.csiro.au
+//    Postal address: Dr. Matthew Whiting
+//                    Australia Telescope National Facility, CSIRO
+//                    PO Box 76
+//                    Epping NSW 1710
+//                    AUSTRALIA
+// -----------------------------------------------------------------------
+
 #include <duchamp/Outputs/CatalogueWriter.hh>
+#include <duchamp/param.hh>
+#include <duchamp/fitsHeader.hh>
+#include <duchamp/Detection/detection.hh>
+#include <duchamp/Outputs/columns.hh>
+#include <duchamp/Utils/Statistics.hh>
+#include <duchamp/Cubes/cubes.hh>
 
 namespace duchamp {
 
