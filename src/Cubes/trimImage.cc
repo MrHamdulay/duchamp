0977322dd3c8317e5a658d8e62b9f2cadea2f7c5
MatthewWhiting <MatthewWhiting@450e7745-72da-457f-8a74-b07dc0cda934>
2014-05-01 05:39:09 +0000

    Alternative trimming function that looks at the blank pixel distribution over *all* channels, not just the first, and uses the spectrumDetect functions to more easily work out ranges of valid pixels.
    git-svn-id: http://svn.atnf.csiro.au/duchamp/trunk@1394 450e7745-72da-457f-8a74-b07dc0cda934

--- src/Cubes/trimImage.cc
+++ src/Cubes/trimImage.cc
@@ -29,158 +29,293 @@
 #include <duchamp/param.hh>
 #include <duchamp/Cubes/cubes.hh>
 #include <duchamp/PixelMap/Object3D.hh>
+#include <duchamp/Detection/finders.hh>
 
 using namespace PixelInfo;
 
 namespace duchamp
 {
 
-  void Cube::trimCube()
-  {
-    /// @details
-    ///  If the blankPix flag has been set, this routine trims excess blank 
-    ///    pixels from the edges of the spatial image.
-    ///   It uses as its template the first channel, assuming that its non-BLANK 
-    ///    size is representative of the rest of the channels.
-    ///   The edges are moved in until the first non-BLANK pixel is encountered.
-    ///   All other arrays are similarly edited, and the amount of trimming is 
-    ///    recorded.
+    void Cube::trimCube()
+    {
 
-    //   if(this->par.getFlagBlankPix()) {
-    if(this->par.getFlagTrim()) {
+	if(this->par.getFlagTrim()) {
 
-      size_t xdim = this->axisDim[0];
-      size_t ydim = this->axisDim[1];
-      size_t zdim = this->axisDim[2];
-      const size_t ZCHAN = 0;
+	    size_t xdim = this->axisDim[0];
+	    size_t ydim = this->axisDim[1];
+	    size_t zdim = this->axisDim[2];
+	    size_t spatsize=xdim*ydim;
+	    
+	    size_t left=0,right=xdim-1,top=ydim-1,bottom=0;
 
-      size_t left,right,top,bottom;
-
-      for(size_t z = 0; z < zdim; z++){
-
-	for(size_t row=0;row<ydim;row++){
-	  left=0;
-	  right=0;
-	  while((left<xdim)&&
-		(this->par.isBlank(this->array[row*xdim+left+ZCHAN*xdim*ydim])) ) {
-	    left++;
-	  }
-	  while((right<xdim)&&
-		(this->par.isBlank(this->array[row*xdim+(xdim-1-right)+ZCHAN*xdim*ydim]))){
-	    right++;
-	  }
-
-	  if( ((z==0)&&(row==0)) || (left < this->par.getBorderLeft()) ) 
-	    this->par.setBorderLeft(left);
-	  if( ((z==0)&&(row==0)) || (right < this->par.getBorderRight()) ) 
-	    this->par.setBorderRight(right);
-
-	}
-    
-	for(size_t col=0;col<xdim;col++){
-	  bottom=0;
-	  top=0;
-	  while((bottom<ydim)&&
-		(this->par.isBlank(this->array[col+bottom*xdim+ZCHAN*xdim*ydim])) ) bottom++;
-
-	  while((top<ydim)&&
-		(this->par.isBlank(this->array[col+(ydim-1-top)*xdim+ZCHAN*xdim*ydim])) ) top++;
-
-	  if( ((z==0)&&(col==0)) || (bottom < this->par.getBorderBottom()) ) 
-	    this->par.setBorderBottom(bottom);
-	  if( ((z==0)&&(col==0)) || (top < this->par.getBorderTop()) ) 
-	    this->par.setBorderTop(top);
-
-	}
-
-      }
-    
-      left = this->par.getBorderLeft();
-      right = this->par.getBorderRight();
-      top = this->par.getBorderTop();
-      bottom = this->par.getBorderBottom();
-
-
-      size_t oldpos,newpos;
-    
-      // Do the trimming, but only if we need to -- is there a border of Blanks?
-      if(((left>0)||(right>0)||(bottom>0)||(top>0)) && (right>left) && (top>bottom)) {
-
-	  this->axisDim[0] = xdim - left - right; 
-	  this->axisDim[1] = ydim - bottom - top; 
-	  this->axisDim[2] = zdim;
-	  this->numPixels = this->axisDim[0]*this->axisDim[1]*this->axisDim[2];
-
-	// Trim the array of pixel values
-	float *newarray  = new float[this->numPixels];
-	for(size_t x = 0; x < axisDim[0]; x++){
-	  for(size_t y = 0; y < axisDim[1]; y++){
-	    for(size_t z = 0; z < axisDim[2]; z++){ 
-	      oldpos = (x+left) + (y+bottom)*xdim + z*xdim*ydim;
-	      newpos = x + y*this->axisDim[0] + 
-		z*this->axisDim[0]*this->axisDim[1];
-	      newarray[newpos]  = this->array[oldpos];
+	    std::vector<bool> spatblank(spatsize,false);
+	    for (size_t z=0;z<zdim;z++){
+		std::vector<bool> currentBlank = this->par.makeBlankMask(this->array+z*spatsize,spatsize);
+		for(size_t i=0;i<spatsize;i++) spatblank[i]=spatblank[i] || currentBlank[i];
 	    }
-	  }
-	}
-	delete [] this->array;
-	this->array = newarray;
 
-	// Trim the array of baseline values
-	if(this->par.getFlagBaseline()){
-	  float *newarray  = new float[this->numPixels];
-	  for(size_t x = 0; x < axisDim[0]; x++){
-	    for(size_t y = 0; y < axisDim[1]; y++){
-	      for(size_t z = 0; z < axisDim[2]; z++){ 
-		oldpos = (x+left) + (y+bottom)*xdim + z*xdim*ydim;
-		newpos = x + y*this->axisDim[0] + 
-		  z*this->axisDim[0]*this->axisDim[1];
-		newarray[newpos]  = this->baseline[oldpos];
-	      }
+	    std::vector<bool> rowblank(xdim,false);
+	    for(size_t x=0;x<xdim;x++){
+		for(size_t y=0;y<ydim;y++){
+		    rowblank[x] = rowblank[x] || spatblank[x+y*xdim];
+		}
 	    }
-	  }
-	  delete [] this->baseline;
-	  this->baseline = newarray;
-	}
+	    std::vector<Scan> rowscans = spectrumDetect(rowblank,xdim,0);
 
-	// Trim the 2-D detection map
-	short *newdetect = new short[this->axisDim[0]*this->axisDim[1]];
-	for(size_t x = 0; x < axisDim[0]; x++){
-	  for(size_t y = 0; y < axisDim[1]; y++){
-	    oldpos = (x+left) + (y+bottom)*xdim;
-	    newpos = x + y*this->axisDim[0];
-	    newdetect[newpos] = this->detectMap[oldpos];
-	  }
-	}
-	delete [] this->detectMap;
-	this->detectMap = newdetect;
-
-	if(this->par.getFlagATrous() || this->par.getFlagSmooth()){
-	  // Trim the reconstructed array if we are going to do the
-	  // reconstruction or smooth the array
-	  float *newrecon  = new float[this->numPixels];
-	  for(size_t x = 0; x < axisDim[0]; x++){
-	    for(size_t y = 0; y < axisDim[1]; y++){
-	      for(size_t z = 0; z < axisDim[2]; z++){ 
-		oldpos = (x+left) + (y+bottom)*xdim + z*xdim*ydim;
-		newpos = x + y*this->axisDim[0] + 
-		  z*this->axisDim[0]*this->axisDim[1];
-		newrecon[newpos] = this->recon[oldpos];	  
-	      }
+	    std::vector<bool> colblank(ydim,false);
+	    for(size_t y=0;y<ydim;y++){
+		for(size_t x=0;x<xdim;x++){
+		    colblank[y] = colblank[y] || spatblank[x+y*xdim];
+		}
 	    }
-	  }
-	  delete [] this->recon;
-	  this->recon = newrecon;
+	    std::vector<Scan> colscans = spectrumDetect(colblank,ydim,0);
+
+
+	    if (rowscans.size()>0 && colscans.size()>0){
+
+		// if these sizes are zero then there are either a row or column of blanks, and we can't trim
+
+		left=rowscans.begin()->getX();
+		right=xdim-1-rowscans.rbegin()->getXmax();
+		bottom=colscans.begin()->getX();
+		top=colscans.rbegin()->getXmax();
+
+		if (left>0 || right>0 || bottom > 0 || top>0 ) {
+		    // only trim if we need to - are the borders right at the edges?
+
+		    // Set the flag indicating trimming has taken place only if it has.
+		    this->par.setFlagCubeTrimmed(true);
+		    this->par.setBorderLeft(left);
+		    this->par.setBorderRight(right);
+		    this->par.setBorderTop(top);
+		    this->par.setBorderBottom(bottom);
+
+		    // Reset the size of the cube
+		    this->axisDim[0] = xdim - left - right; 
+		    this->axisDim[1] = ydim - bottom - top; 
+		    this->axisDim[2] = zdim;
+		    this->numPixels = this->axisDim[0]*this->axisDim[1]*this->axisDim[2];
+
+		    size_t oldpos,newpos;
+
+		    // Trim the array of pixel values
+		    float *newarray  = new float[this->numPixels];
+		    for(size_t x = 0; x < axisDim[0]; x++){
+			for(size_t y = 0; y < axisDim[1]; y++){
+			    for(size_t z = 0; z < axisDim[2]; z++){ 
+				oldpos = (x+left) + (y+bottom)*xdim + z*xdim*ydim;
+				newpos = x + y*this->axisDim[0] + 
+				    z*this->axisDim[0]*this->axisDim[1];
+				newarray[newpos]  = this->array[oldpos];
+			    }
+			}
+		    }
+		    delete [] this->array;
+		    this->array = newarray;
+
+		    // Trim the array of baseline values
+		    if(this->par.getFlagBaseline()){
+			float *newarray  = new float[this->numPixels];
+			for(size_t x = 0; x < axisDim[0]; x++){
+			    for(size_t y = 0; y < axisDim[1]; y++){
+				for(size_t z = 0; z < axisDim[2]; z++){ 
+				    oldpos = (x+left) + (y+bottom)*xdim + z*xdim*ydim;
+				    newpos = x + y*this->axisDim[0] + 
+					z*this->axisDim[0]*this->axisDim[1];
+				    newarray[newpos]  = this->baseline[oldpos];
+				}
+			    }
+			}
+			delete [] this->baseline;
+			this->baseline = newarray;
+		    }
+
+		    // Trim the 2-D detection map
+		    short *newdetect = new short[this->axisDim[0]*this->axisDim[1]];
+		    for(size_t x = 0; x < axisDim[0]; x++){
+			for(size_t y = 0; y < axisDim[1]; y++){
+			    oldpos = (x+left) + (y+bottom)*xdim;
+			    newpos = x + y*this->axisDim[0];
+			    newdetect[newpos] = this->detectMap[oldpos];
+			}
+		    }
+		    delete [] this->detectMap;
+		    this->detectMap = newdetect;
+
+		    if(this->par.getFlagATrous() || this->par.getFlagSmooth()){
+			// Trim the reconstructed array if we are going to do the
+			// reconstruction or smooth the array
+			float *newrecon  = new float[this->numPixels];
+			for(size_t x = 0; x < axisDim[0]; x++){
+			    for(size_t y = 0; y < axisDim[1]; y++){
+				for(size_t z = 0; z < axisDim[2]; z++){ 
+				    oldpos = (x+left) + (y+bottom)*xdim + z*xdim*ydim;
+				    newpos = x + y*this->axisDim[0] + 
+					z*this->axisDim[0]*this->axisDim[1];
+				    newrecon[newpos] = this->recon[oldpos];	  
+				}
+			    }
+			}
+			delete [] this->recon;
+			this->recon = newrecon;
+		    }
+
+		}
+
+	    }
+
 	}
-
-	// Set the flag indicating trimming has taken place only if it has.
-	this->par.setFlagCubeTrimmed(true);
-
-      }
 
     }
 
-  }
+
+
+  // void Cube::trimCubeOld()
+  // {
+  //   /// @details
+  //   ///  If the blankPix flag has been set, this routine trims excess blank 
+  //   ///    pixels from the edges of the spatial image.
+  //   ///   It uses as its template the first channel, assuming that its non-BLANK 
+  //   ///    size is representative of the rest of the channels.
+  //   ///   The edges are moved in until the first non-BLANK pixel is encountered.
+  //   ///   All other arrays are similarly edited, and the amount of trimming is 
+  //   ///    recorded.
+
+  //   //   if(this->par.getFlagBlankPix()) {
+  //   if(this->par.getFlagTrim()) {
+
+  //     size_t xdim = this->axisDim[0];
+  //     size_t ydim = this->axisDim[1];
+  //     size_t zdim = this->axisDim[2];
+  //     const size_t ZCHAN = 0;
+
+  //     size_t left,right,top,bottom;
+
+  //     for(size_t z = 0; z < zdim; z++){
+
+  // 	for(size_t row=0;row<ydim;row++){
+  // 	  left=0;
+  // 	  right=0;
+  // 	  while((left<xdim)&&
+  // 		(this->par.isBlank(this->array[row*xdim+left+ZCHAN*xdim*ydim])) ) {
+  // 	    left++;
+  // 	  }
+  // 	  while((right<xdim)&&
+  // 		(this->par.isBlank(this->array[row*xdim+(xdim-1-right)+ZCHAN*xdim*ydim]))){
+  // 	    right++;
+  // 	  }
+
+  // 	  if( ((z==0)&&(row==0)) || (left < this->par.getBorderLeft()) ) 
+  // 	    this->par.setBorderLeft(left);
+  // 	  if( ((z==0)&&(row==0)) || (right < this->par.getBorderRight()) ) 
+  // 	    this->par.setBorderRight(right);
+
+  // 	}
+    
+  // 	for(size_t col=0;col<xdim;col++){
+  // 	  bottom=0;
+  // 	  top=0;
+  // 	  while((bottom<ydim)&&
+  // 		(this->par.isBlank(this->array[col+bottom*xdim+ZCHAN*xdim*ydim])) ) bottom++;
+
+  // 	  while((top<ydim)&&
+  // 		(this->par.isBlank(this->array[col+(ydim-1-top)*xdim+ZCHAN*xdim*ydim])) ) top++;
+
+  // 	  if( ((z==0)&&(col==0)) || (bottom < this->par.getBorderBottom()) ) 
+  // 	    this->par.setBorderBottom(bottom);
+  // 	  if( ((z==0)&&(col==0)) || (top < this->par.getBorderTop()) ) 
+  // 	    this->par.setBorderTop(top);
+
+  // 	}
+
+  //     }
+    
+  //     left = this->par.getBorderLeft();
+  //     right = this->par.getBorderRight();
+  //     top = this->par.getBorderTop();
+  //     bottom = this->par.getBorderBottom();
+
+
+  //     size_t oldpos,newpos;
+    
+  //     // Do the trimming, but only if we need to -- is there a border of Blanks?
+  //     if(((left>0)||(right>0)||(bottom>0)||(top>0)) && (right>left) && (top>bottom)) {
+
+  // 	  this->axisDim[0] = xdim - left - right; 
+  // 	  this->axisDim[1] = ydim - bottom - top; 
+  // 	  this->axisDim[2] = zdim;
+  // 	  this->numPixels = this->axisDim[0]*this->axisDim[1]*this->axisDim[2];
+
+  // 	// Trim the array of pixel values
+  // 	float *newarray  = new float[this->numPixels];
+  // 	for(size_t x = 0; x < axisDim[0]; x++){
+  // 	  for(size_t y = 0; y < axisDim[1]; y++){
+  // 	    for(size_t z = 0; z < axisDim[2]; z++){ 
+  // 	      oldpos = (x+left) + (y+bottom)*xdim + z*xdim*ydim;
+  // 	      newpos = x + y*this->axisDim[0] + 
+  // 		z*this->axisDim[0]*this->axisDim[1];
+  // 	      newarray[newpos]  = this->array[oldpos];
+  // 	    }
+  // 	  }
+  // 	}
+  // 	delete [] this->array;
+  // 	this->array = newarray;
+
+  // 	// Trim the array of baseline values
+  // 	if(this->par.getFlagBaseline()){
+  // 	  float *newarray  = new float[this->numPixels];
+  // 	  for(size_t x = 0; x < axisDim[0]; x++){
+  // 	    for(size_t y = 0; y < axisDim[1]; y++){
+  // 	      for(size_t z = 0; z < axisDim[2]; z++){ 
+  // 		oldpos = (x+left) + (y+bottom)*xdim + z*xdim*ydim;
+  // 		newpos = x + y*this->axisDim[0] + 
+  // 		  z*this->axisDim[0]*this->axisDim[1];
+  // 		newarray[newpos]  = this->baseline[oldpos];
+  // 	      }
+  // 	    }
+  // 	  }
+  // 	  delete [] this->baseline;
+  // 	  this->baseline = newarray;
+  // 	}
+
+  // 	// Trim the 2-D detection map
+  // 	short *newdetect = new short[this->axisDim[0]*this->axisDim[1]];
+  // 	for(size_t x = 0; x < axisDim[0]; x++){
+  // 	  for(size_t y = 0; y < axisDim[1]; y++){
+  // 	    oldpos = (x+left) + (y+bottom)*xdim;
+  // 	    newpos = x + y*this->axisDim[0];
+  // 	    newdetect[newpos] = this->detectMap[oldpos];
+  // 	  }
+  // 	}
+  // 	delete [] this->detectMap;
+  // 	this->detectMap = newdetect;
+
+  // 	if(this->par.getFlagATrous() || this->par.getFlagSmooth()){
+  // 	  // Trim the reconstructed array if we are going to do the
+  // 	  // reconstruction or smooth the array
+  // 	  float *newrecon  = new float[this->numPixels];
+  // 	  for(size_t x = 0; x < axisDim[0]; x++){
+  // 	    for(size_t y = 0; y < axisDim[1]; y++){
+  // 	      for(size_t z = 0; z < axisDim[2]; z++){ 
+  // 		oldpos = (x+left) + (y+bottom)*xdim + z*xdim*ydim;
+  // 		newpos = x + y*this->axisDim[0] + 
+  // 		  z*this->axisDim[0]*this->axisDim[1];
+  // 		newrecon[newpos] = this->recon[oldpos];	  
+  // 	      }
+  // 	    }
+  // 	  }
+  // 	  delete [] this->recon;
+  // 	  this->recon = newrecon;
+  // 	}
+
+  // 	// Set the flag indicating trimming has taken place only if it has.
+  // 	this->par.setFlagCubeTrimmed(true);
+
+  //     }
+
+  //   }
+
+  // }
 
 
   void Cube::unTrimCube()
