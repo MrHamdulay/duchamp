7ac86e233c3e40e5b3ae555867fd835bbc77cbcb
MatthewWhiting <MatthewWhiting@450e7745-72da-457f-8a74-b07dc0cda934>
2014-05-28 04:44:55 +0000

    Fixing the interfaces for the Detection::addOffsets functions (setting the offsets to long instead of size_t to match the Object3D declaration).
    git-svn-id: http://svn.atnf.csiro.au/duchamp/trunk@1416 450e7745-72da-457f-8a74-b07dc0cda934

--- src/Detection/detection.hh
+++ src/Detection/detection.hh
@@ -109,17 +109,15 @@ namespace duchamp
     /// @brief Set the values of the axis offsets from the cube. 
     void   setOffsets(Param &par); 
 
-      using Object3D::addOffsets;  //tell the compiler we want both the addOffsets from Object3D *and* Detection
-
-    /// @brief Add the offset values to the pixel locations 
-   void   addOffsets(size_t xoff, size_t yoff, size_t zoff){
-       Object3D::addOffsets(xoff,yoff,zoff);
-       xpeak+=xoff; ypeak+=yoff; zpeak+=zoff;
-       xCentroid+=xoff; yCentroid+=yoff; zCentroid+=zoff;
-    };
+      /// @brief Add the offset values to the pixel locations 
+      void   addOffsets(long xoff, long yoff, long zoff){
+	  Object3D::addOffsets(xoff,yoff,zoff);
+	  xpeak+=xoff; ypeak+=yoff; zpeak+=zoff;
+	  xCentroid+=xoff; yCentroid+=yoff; zCentroid+=zoff;
+      };
 
       void   addOffsets(){ addOffsets(xSubOffset, ySubOffset, zSubOffset);};
-      void   removeOffsets(size_t xoff, size_t yoff, size_t zoff){ addOffsets(-xoff, -yoff, -zoff);};
+      void   removeOffsets(long xoff, long yoff, long zoff){ addOffsets(-xoff, -yoff, -zoff);};
       void   removeOffsets(){ addOffsets(-xSubOffset, -ySubOffset, -zSubOffset);};
       void   addOffsets(Param &par){setOffsets(par); addOffsets();};
 
