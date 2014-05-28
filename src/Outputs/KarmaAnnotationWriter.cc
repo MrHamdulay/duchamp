a72facbec9ad758261d91e51a6c08ab3d4323302
MatthewWhiting <MatthewWhiting@450e7745-72da-457f-8a74-b07dc0cda934>
2014-05-28 04:20:03 +0000

    For the Karma annotation files, reverting back to using individual lines (with LINE), rather than join the dots with CLINES. The reason for this is that CLINES has some built-in limit that prevents very large perimeters from being drawn.
    git-svn-id: http://svn.atnf.csiro.au/duchamp/trunk@1415 450e7745-72da-457f-8a74-b07dc0cda934

--- src/Outputs/KarmaAnnotationWriter.cc
+++ src/Outputs/KarmaAnnotationWriter.cc
@@ -118,9 +118,13 @@ namespace duchamp {
   {
     if(this->itsOpenFlag){
       if(x.size()==y.size()){
-	this->itsFileStream << "CLINES";
-	for(size_t i=0;i<x.size();i++) this->itsFileStream <<" " << x[i] << " " << y[i];
-	this->itsFileStream << "\n";
+	// this->itsFileStream << "CLINES";
+	// for(size_t i=0;i<x.size();i++) this->itsFileStream <<" " << x[i] << " " << y[i];
+	// this->itsFileStream << "\n";
+	  
+	  for(size_t i=0;i<x.size()-1;i++) this->line(x[i],x[i+1],y[i],y[i+1]);
+	  this->line(x[x.size()-1],x[0],y[x.size()-1],y[0]);
+
       }
     }
   }
